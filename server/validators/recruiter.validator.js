ae3254219e8ab2c42048589a161b26b38ff2d005
SNEHITHKALLUMMAL <snehithkallummal999@gmail.com>
2026-08-21 10:46:10 +0530

    Delete server directory

--- server/validators/recruiter.validator.js
+++
@@ -1,8 +0,0 @@
-import { body } from 'express-validator';
-
-export const updateRecruiterValidator = [
-  body('companyName').optional().trim().notEmpty().isLength({ max: 150 }),
-  body('companyLogoUrl').optional({ values: 'falsy' }).isURL(),
-  body('companyWebsite').optional({ values: 'falsy' }).isURL(),
-  body('industry').optional({ values: 'falsy' }).trim().isLength({ max: 100 }),
-];
--- server/validators/report.validator.js
+++
@@ -1,8 +0,0 @@
-import { body } from 'express-validator';
-import { REPORT_SCOPES } from '../models/Report.model.js';
-
-export const generateReportValidator = [
-  body('scope')
-    .isIn(REPORT_SCOPES)
-    .withMessage(`scope must be one of: ${REPORT_SCOPES.join(', ')}`),
-];
