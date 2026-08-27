b94af36ed1025ea870a7209e9e4ae3b2d9adc9fb
SNEHITHKALLUMMAL <snehithkallummal999@gmail.com>
2026-08-27 11:27:09 +0530

    Delete server directory

--- server/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-export default {
-  testEnvironment: 'node',
-  transform: {},
-  testMatch: ['**/tests/**/*.test.js'],
-  setupFiles: ['<rootDir>/tests/setup.env.js'],
-  verbose: true,
-  clearMocks: true,
-};
